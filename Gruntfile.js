720a70042fe37e4c8c41f9ac0afdbbf933972d22
Simon Harte <simon.harte@namics.com>
2015-06-24 15:52:10 +0200

    update grunt-at-imports

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
 /*
- * grunt-less-imports
- * https://github.com/MarcDiethelm/grunt-less-imports
+ * grunt-at-imports
+ * https://github.com/SimonHarte/grunt-at-imports
  *
- * Copyright (c) 2013 Marc Diethelm
+ * Copyright (c) 2013 Simon Harte
  * Licensed under the MIT license.
  */
 
@@ -18,8 +18,7 @@ module.exports = function (grunt) {
 		jshint: {
 			all: [
 				'Gruntfile.js',
-				'tasks/*.js',
-				'<%= nodeunit.tests %>',
+				'tasks/*.js'
 			],
 			options: {
 				jshintrc: '.jshintrc'
@@ -32,47 +31,133 @@ module.exports = function (grunt) {
 		},
 
 		// Configuration to be run (and then tested).
-		less_imports: {
-			test_src_dest: {
+		at_imports: {
+			test_src_dest_less: {
 				options: {
 				},
-				src: ['test/fixtures/*.less', 'test/fixtures/*.css', 'test/fixtures/deeper/*.less', 'test/fixtures/deeper/*.css'], // test using incorrect order, because we handle this
+				src: [
+					'test/fixtures/*.less',
+					'test/fixtures/*.css',
+					'test/fixtures/deeper/*.less',
+					'test/fixtures/deeper/*.css'
+				], // test using incorrect order, because we handle this
 				dest: 'tmp/test_src_dest/imports.less'
 			},
-			test_files: {
+			test_src_dest_sass: {
+				options: {
+				},
+				src: [
+					'test/fixtures/*.scss',
+					'test/fixtures/*.css',
+					'test/fixtures/deeper/*.scss',
+					'test/fixtures/deeper/*.css'
+				], // test using incorrect order, because we handle this
+				dest: 'tmp/test_src_dest/imports.scss'
+			},
+			test_files_less: {
 				options: {
 				},
 				files: {
-					'tmp/test_files/imports.less': ['test/fixtures/styles.less', 'test/fixtures/styles.css', 'test/fixtures/deeper/styles.less', 'test/fixtures/deeper/styles.css']
+					'tmp/test_files/imports.less': [
+						'test/fixtures/styles.less',
+						'test/fixtures/styles.css',
+						'test/fixtures/deeper/styles.less',
+						'test/fixtures/deeper/styles.css'
+					]
 				}
 			},
-			test_more_files: {
+			test_files_sass: {
 				options: {
 				},
 				files: {
-					'tmp/test_more_files/default.less': ['test/fixtures/styles.less', 'test/fixtures/styles.css'],
-					'tmp/test_more_files/deeper.less': ['test/fixtures/deeper/styles.less', 'test/fixtures/deeper/styles.css']
+					'tmp/test_files/imports.scss': [
+						'test/fixtures/styles.scss',
+						'test/fixtures/styles.css',
+						'test/fixtures/deeper/styles.scss',
+						'test/fixtures/deeper/styles.css'
+					]
 				}
 			},
-			inline_css_false: {
+			test_more_files_less: {
+				options: {
+				},
+				files: {
+					'tmp/test_more_files/default.less': [
+						'test/fixtures/styles.less',
+						'test/fixtures/styles.css'
+					],
+					'tmp/test_more_files/deeper.less': [
+						'test/fixtures/deeper/styles.less',
+						'test/fixtures/deeper/styles.css'
+					]
+				}
+			},
+			test_more_files_sass: {
+				options: {
+				},
+				files: {
+					'tmp/test_more_files/default.scss': [
+						'test/fixtures/styles.scss',
+						'test/fixtures/styles.css'
+					],
+					'tmp/test_more_files/deeper.scss': [
+						'test/fixtures/deeper/styles.scss',
+						'test/fixtures/deeper/styles.css'
+					]
+				}
+			},
+			inline_css_false_less: {
 				options: {
 					inlineCSS: false
 				},
-				src: ['test/fixtures/*.less', 'test/fixtures/*.css', 'test/fixtures/deeper/*.less', 'test/fixtures/deeper/*.css'],
+				src: [
+					'test/fixtures/*.less',
+					'test/fixtures/*.css',
+					'test/fixtures/deeper/*.less',
+					'test/fixtures/deeper/*.css'
+				],
 				dest: 'tmp/inline_css_false/imports.less'
 			},
-			test_custom_banner: {
+			inline_css_false_sass: {
+				options: {
+					inlineCSS: false
+				},
+				src: [
+					'test/fixtures/*.scss',
+					'test/fixtures/*.css',
+					'test/fixtures/deeper/*.scss',
+					'test/fixtures/deeper/*.css'
+				],
+				dest: 'tmp/inline_css_false/imports.scss'
+			},
+			test_custom_banner_less: {
 				options: {
 					banner: '// Banner for <%= package.name %>'
 				},
-				src: ['test/fixtures/*.less', 'test/fixtures/*.css'],
+				src: [
+					'test/fixtures/*.less',
+					'test/fixtures/*.css'
+				],
 				dest: 'tmp/test_custom_banner/imports.less'
 			},
-			import_option: {
+			test_custom_banner_sass: {
+				options: {
+					banner: '// Banner for <%= package.name %>'
+				},
+				src: [
+					'test/fixtures/*.scss',
+					'test/fixtures/*.css'
+				],
+				dest: 'tmp/test_custom_banner/imports.scss'
+			},
+			import_option_less: {
 				options: {
 					import: 'reference'
 				},
-				src: ['test/fixtures/*.less', 'test/fixtures/*.css'],
+				src: [
+					'test/fixtures/*.less',
+					'test/fixtures/*.css'
+				],
 				dest: 'tmp/import_options/imports.less'
 			},
 			import_function_option: {
@@ -80,12 +165,17 @@ module.exports = function (grunt) {
 					import: function(filepath, extension) {
 						if (filepath === 'test/fixtures/styles.less') {
 							return 'reference';
+						} else {
+							return 'once';
 						}
-						else return 'once';
 					}
 				},
 				files: {
-					'tmp/import_options_callback/imports.less': ['test/fixtures/styles.less', 'test/fixtures/styles.css', 'test/fixtures/deeper/*.less']
+					'tmp/import_options_callback/imports.less': [
+						'test/fixtures/styles.less',
+						'test/fixtures/styles.css',
+						'test/fixtures/deeper/*.less'
+					]
 				}
 			}
 		},
@@ -103,8 +193,17 @@ module.exports = function (grunt) {
 		less: {
 			compile: {
 				files: {
-					'tmp/compiled/test_default.css': 'tmp/test_src_dest/imports.less',
-					'tmp/compiled/inline_css_false.css': 'tmp/inline_css_false/imports.less'
+					'tmp/compiled/test_default.less.css': 'tmp/test_src_dest/imports.less',
+					'tmp/compiled/inline_css_false.less.css': 'tmp/inline_css_false/imports.less'
+				}
+			}
+		},
+
+		sass: {
+			compile: {
+				files: {
+					'tmp/compiled/test_default.scss.css': 'tmp/test_src_dest/imports.scss',
+					'tmp/compiled/inline_css_false.scss.css': 'tmp/inline_css_false/imports.scss'
 				}
 			}
 		}
@@ -118,11 +217,12 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-mocha-test');
 
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
 	// plugin's task(s), then test the result.
-	grunt.registerTask('test', ['clean', 'less_imports', 'less', 'mochaTest']);
+	grunt.registerTask('test', ['clean', 'at_imports', 'less', 'sass', 'mochaTest']);
 
 	// By default, lint and run all tests.
 	grunt.registerTask('default', ['jshint', 'test']);
